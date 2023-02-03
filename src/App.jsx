520e43ebeef353ac08bbc5bd8c886295800b6eab
Alex R. Farnes <alexrfarnes@gmail.com>
2023-02-03 14:23:44 +0800

    Add the routes for the edit and 404 pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import RootLayout from './layouts/RootLayout';
+import Edit from './pages/Edit';
 import Home from './pages/Home';
+import Page404 from './pages/Page404';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<RootLayout />}>
         <Route index element={<Home />} />
+        <Route path='/:id/edit' element={<Edit />} />
+        <Route path='*' element={<Page404 />} />
       </Route>
     )
   );

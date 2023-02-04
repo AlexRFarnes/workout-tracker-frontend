3e731b5ea6531cb4be2be385c17a994972ca783b
Alex R. Farnes <alexrfarnes@gmail.com>
2023-02-04 09:15:41 +0800

    Add new variables to the gobal styles and create primary and secondary buttons

--- src/styles/Button.styled.jsx
+++ src/styles/Button.styled.jsx
@@ -4,9 +4,7 @@ export const Button = styled.button.attrs(({ type, disabled }) => ({
   type: type || 'button',
   disabled: disabled || false,
 }))`
-  background: var(--primary-color);
   border: 3px solid transparent;
-  color: #fff;
   padding: 10px;
   font-family: 'Poppins';
   border-radius: 4px;
@@ -14,9 +12,33 @@ export const Button = styled.button.attrs(({ type, disabled }) => ({
   transition: all 0.2s ease-in;
   transition-property: background, color, border-color;
 
-  &:hover {
+  &:disabled,
+  &[disabled] {
+    border-color: var(--medium-gray-color);
+    background-color: var(--light-gray-color);
+    color: var(--dark-color);
+    cursor: not-allowed;
+  }
+`;
+
+export const PrimaryButton = styled(Button)`
+  color: #fff;
+  background: var(--primary-color);
+
+  &:hover:not([disabled]) {
     background: transparent;
     color: var(--primary-color);
     border-color: var(--primary-color);
+  }
+`;
+
+export const SecondaryButton = styled(Button)`
+  color: var(--light-color);
+  background: var(--dark-color);
+
+  &:hover:not([disabled]) {
+    background: transparent;
+    color: var(--dark-color);
+    border-color: var(--dark-color);
   }
 `;
--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -6,10 +6,15 @@ const GlobalStyles = createGlobalStyle`
         --primary-color-darker: #545cf2;
         --error: #e7195a;
         --success: #1aac83;
-        --light-color: #f3f3f3;
+        --light-color: #fafafa;
         --light-gray-color: #e7e7e7;
+        --medium-gray-color: #aaa;
         --dark-gray-color: #555;
         --dark-color: #333;
+
+        --border-radius: 4px;
+
+        --box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.05);
     }
 
     *,

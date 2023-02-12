de351d0dbe01d7ebc34b7fda37b7aa0b454075f2
Alex R. Farnes <alexrfarnes@gmail.com>
2023-02-12 11:10:07 +0800

    Clean the workouts context during logout

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,10 +1,13 @@
 import { useAuthContext } from './useAuthContext';
+import { useWorkoutsContext } from './useWorkoutsContext';
 
 export const useLogout = () => {
+  const { dispatch: dispatchWorkout } = useWorkoutsContext();
   const { dispatch } = useAuthContext();
 
   const logout = () => {
     dispatch({ type: 'LOGOUT' });
+    dispatchWorkout({ type: 'SET_WORKOUTS', payload: null });
     localStorage.removeItem('WORKOUTS_TRACKER_user');
   };
 
